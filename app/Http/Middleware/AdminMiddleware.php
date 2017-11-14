902a60dbad1bacb6a0a644de22f4fd8a5c539ac6
= <=>
2017-11-14 14:20:12 +0100

    Comments associated with the deleted user will now vanish as well

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::id() != 1) {
+        if (Auth::id() !== 1) {
             return redirect('/');
         }
 
